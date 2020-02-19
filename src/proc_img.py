c0f44881e7b331edfe6a9bfe86f1c8ba693f6b5e
mateuslenier <mateuslenier@gmail.com>
2020-02-19 00:07:19 -0300

    Update on procImg class, association with genDummy WIP

--- src/proc_img.py
+++ src/proc_img.py
@@ -26,32 +26,25 @@ class procImg:
         self.spot_radius = spot_radius
         self.channels_bias = channels_bias
     
-    def imread(self):
-    
-        return None
-    
-    def gaussian_otsu_threshold_conv(self, img):
+    @staticmethod
+    def find_countours_img(img: list) -> list:
         
-        gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+        kernel = np.ones((3, 3, np.uint8))
+
+        if len(img) == 3:
+            gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
         
         blur = cv.GaussianBlur(gray, (5, 5), 0)
-        ret, thresh = cv.threshold(gray, 0, 255, cv.THRESH_BINARY + cv.THRESH_OTSU)
-
-        return cv.bitwise_not(thresh)
-    
-    def img_dilation(self, thresh):
+        ret, thresh = cv.threshold(blur, 0, 255, cv.THRESH_BINARY + cv.THRESH_OTSU)
         
-        kernel = np.ones((5, 5, np.uint8))
+        dilation = cv.dilate(cv.bitwise_not(thresh),kernel, iterations=1)
         
-        return cv.dilate(thresh,kernel, iterations=1)
-    
-    def find_countours_img(self, dilation):
-
         contours, hierarchy = cv.findContours(dilation, cv.RETR_TREE,cv.CHAIN_APPROX_TC89_KCOS)
-
-        return contours
+        
+        return contours 
     
-    def get_biggest_lesion(self, contours):
+    @staticmethod
+    def get_biggest_lesion(contours):
         
         cnt = []
         contour_area = 0
@@ -66,7 +59,8 @@ class procImg:
         
         return cnt
     
-    def rect_img(self, cnt, img):
+    @staticmethod
+    def rect_img(cnt, img: list) -> tuple:
         
         left = img.shape[1]
         top = img.shape[0]
@@ -88,13 +82,10 @@ class procImg:
                     bottom = y
         return (left, top, right, bottom)
     
-    def draw_countors_img(self, img, cnt, rgb: tuple, thicc: int):
+    @staticmethod
+    def grabcut_img(img, rect, cnt, rgb = (0, 255, 0), thicc = 1) -> list:
         
         cv.drawContours(img, cnt, -1, rgb, thicc)
-
-        return
-    
-    def grabcut_img(self, img, rect):
         
         mask = np.zeros(img.shape[:2], np.uint8)
 
@@ -106,5 +97,103 @@ class procImg:
         mask2 = np.where((mask==2)|(mask==0),0,1).astype('uint8')
         
         return img*mask2[:,:,np.newaxis]
+    
+    @staticmethod
+    def imshow (self, img):
+        #Verify if the img is RGB and converts it
+        if (hasattr(img[0][0], '__len__')):
+            plt.imshow (img, cmap='gray', interpolation= 'nearest')
+        else:
+            plt.imshow (img, interpolation='nearest')
+        plt.show()
+    
+    def get_vals_spot(self, img:list):
+        
+        ret = [[]for x in range(3)]
 
+        for i in range(self.n):
+            for j in range(self.n):
+                if(not 0 in img[i][j] and not 255 in img[i][j]):
+                    for channel in range(len(ret)):
+                        ret[channel].append(img[i][j][channel])
+        
+        return ret
+    
+    @staticmethod
+    def get_ga_list_spot(img: list) -> list:
+        
+        ret = [[0 for x in range(256)] for x in range(3)]
+
+        for i in range(len(img)):
+            for j in range(len(img[0])):
+                if(not 0 in img[i][j] and not 255 in img[i][j]):
+                    for channel in range(len(ret)):
+                        ret[channel][img[i][j][channel]]+=1
+        
+        return ret
+    
+    def generate_img(self, apply_bias: bool):
+        img = np.array([[self.skin_color for _ in range(self.n)] for _ in range(self.n)], dtype=np.uint8)
+        cell_center = [self.n//2 + random.randint(-8, 8), self.n//2 + random.randint(-8, 8)]
+        for i in range(self.n):
+            for j in range(self.n):
+                euclidean_distance = int(np.linalg.norm(cell_center-np.array([i, j])))
+                if(euclidean_distance < self.spot_radius+1):
+                    if(euclidean_distance > self.spot_radius-2):
+                        img[i][j] = self.spot_border_color
+                    else:
+                        for channel in range(3):
+                            min_now = self.min_val
+                            max_now = self.max_val
+                            if(apply_bias and self.channels_bias[channel] and random.random() < self.odd_bias):
+                                min_now = self.min_bias
+                                max_now = self.max_bias
+                            img[i][j][channel] = random.randint(min_now, max_now)
+
+        return img
+
+    def generate_img_batch(self, folder_name: str, qnt: int, apply_bias: bool):
+        """Generate multiple images based on parameters. Images will be saved in
+            ../img/folder_name as jsons, uncompressed."""
+        img_path = os.path.join('..', 'img')
+        if(not os.path.exists(img_path)):
+            os.makedirs(img_path)
+        if(not os.path.exists(os.path.join(img_path, folder_name))):
+            os.makedirs(os.path.join(img_path, folder_name))
+
+        if (apply_bias):
+            pref = "sick_"
+        else:
+            pref = "health_"
+        for i in tqdm(range(qnt)):
+            with open(os.path.join(img_path, folder_name, pref + str(i) + '.json'), 'w') as fl:
+                json.dump(self.generate_img(apply_bias).tolist(), fl)
+
+
+    @staticmethod
+    def generate_dataset(zip_name : str) -> None:
+        img_path = os.path.join('..', 'img')
+        if(not os.path.exists(img_path)):
+            return None
+        dataset = []
+        with zipfile.ZipFile(os.path.join(img_path, zip_name + '.zip'), 'r') as z:
+            lst = z.namelist()
+            for i in tqdm(range(len(lst))):
+                filename = lst[i]
+                if(filename[-1] != '/'):
+                    with z.open(filename) as fl:
+                        img = np.array(json.loads(fl.read().decode('utf-8')))
+                        dataset.append([procImg.get_ga_list_spot(img), 'sick' in filename])
+        with open(os.path.join(img_path, "ga_list_" + zip_name + '.json'), 'w') as fl:
+            json.dump(dataset, fl)
+
+    @staticmethod
+    def get_dataset(id : str) -> list:
+        """Loads requisited dataset from json file."""
+        fl_path = os.path.join('..', 'img', id + '.json')
+        if(os.path.exists(fl_path)):
+            with open(fl_path, 'r') as fl:
+                return json.load(fl)
+        else:
+            return None
            
